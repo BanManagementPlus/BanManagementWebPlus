d0d5b13df61ef18748eed271f3f0c164a682869d
Mryan <40586009+Mryan2005@users.noreply.github.com>
2019-03-17 22:43:28 +0800

    Update settings.php

--- settings.php
+++ settings.php
@@ -3,7 +3,7 @@ $settings['utf8'] = true;							#请与服务器插件配置一致
 $settings['latest_bans'] = true;					#请与服务器插件配置一致
 $settings['latest_mutes'] = true;					#请与服务器插件配置一致
 $settings['servers'] = '';							#无需更改
-$settings['password'] = '';							#在这里设置你的管理员密码
+$settings['password'] = '123';							#在这里设置你的管理员密码
 $settings['footer'] = '';			#版权修改,例如：(C) 2016 HayoStudio
-$settings['multi-user'] = 'false';
+$settings['multi_user'] = 'false';
 ?>
