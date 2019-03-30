082a465b63b6c7be0204dbe0e1ce72d9fadf7279
Mryan <40586009+Mryan2005@users.noreply.github.com>
2019-03-30 11:32:22 +0800

    Update settings.php

--- settings.php
+++ settings.php
@@ -3,8 +3,9 @@ $settings['utf8'] = true;							#请与服务器插件配置一致
 $settings['latest_bans'] = true;					#请与服务器插件配置一致
 $settings['latest_mutes'] = true;					#请与服务器插件配置一致
 $settings['servers'] = '';							#无需更改
-$settings['website_title'] = '';                    #网站标题栏的标题
 $settings['password'] = '';							#在这里设置你的管理员密码
+// 关于网站的设置
+$settings['website_title'] = '';                    #网站标题栏的标题
 $settings['footer'] = '';			                #页脚修改,例如：(C) 2016 HayoStudio
 $settings['your_url_of_minecraft_server'] = '';     #页脚链接,例如：https://www.baidu.com
 ?>
