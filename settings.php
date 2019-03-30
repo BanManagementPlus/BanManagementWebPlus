315ed224b538dc895ed0ef56db29b114f933285c
Mryan <40586009+Mryan2005@users.noreply.github.com>
2019-03-30 12:31:41 +0800

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,11 +1,12 @@
 <?php
+// 后台设置
 $settings['utf8'] = true;							#请与服务器插件配置一致
 $settings['latest_bans'] = true;					#请与服务器插件配置一致
 $settings['latest_mutes'] = true;					#请与服务器插件配置一致
 $settings['servers'] = '';							#无需更改
 $settings['password'] = '';							#在这里设置你的管理员密码
-// 关于网站的设置
-$settings['website_title'] = '';                    #网站标题栏的标题
+// 网站设置
+$settings['website_title'] = '';                    #网站标题栏的标题,如果为空,内容将会"Ban Management - 玩家封禁系统"
 $settings['footer'] = '';			                #页脚修改,例如：(C) 2016 HayoStudio
 $settings['your_url_of_minecraft_server'] = '';     #页脚链接,例如：https://www.baidu.com
 ?>
