b7a45ab74b9b7186ed8fafa6a2fc36a42aec03ae
Mryan <40586009+Mryan2005@users.noreply.github.com>
2019-04-06 14:35:05 +0800

    add zh-CN

---
+++ language/zh-CN.php
@@ -0,0 +1,19 @@
+<?php
+$language['login'] = '登录';
+$language['Server_Name'] = '服务器名称';
+$language['Options'] = '选项';
+$language['Add_Server'] = '添加服务器';
+$language['MySQL_Host'] = '数据库地址';
+$language['MySQL_Database'] = '数据库名';
+$language['MySQL_Username'] = '数据库用户名';
+$language['MySQL_Password'] = '数据库密码';
+$language['Bans_Table'] = '封禁数据表名';
+$language['Bans_Record_Table'] = '封禁记录数据表名';
+$language['IP_Bans_Table'] = 'IP封禁数据表名';
+$language['IP_Record_Table'] = 'IP记录数据表名';
+$language['Mutes_Table'] = '禁言数据表名';
+$language['Mutes_Record_Table'] = '禁言记录数据表名';
+$language['Kicks_Table'] = '踢出数据表名';
+$language['Warnings_Table'] = '警告数据表名';
+$language['Close'] = '关闭';
+?>
