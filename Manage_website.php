58dc38268f3940398ebf2641e46a6a4211d0adbd
Mryan <40586009+Mryan2005@users.noreply.github.com>
2019-05-29 20:01:46 +0800

    fix bugs

--- Manage_website.php
+++ Manage_website.php
@@ -1,3 +1,6 @@
+<?php
+if(isset($_SESSION['admin']) && $_SESSION['admin']) {
+?>
 <h4>
     <img src="img/chilun.jpg" width="30" height="30" />
     <a href="index.php?action=ConfigureSystem"><?php echo $language['Configure_System']; ?></a>
@@ -9,3 +12,6 @@
     <a href="index.php?action=Server_Settings"><?php echo $language['Server_Settings']; ?></a>
     <h5><?php echo $language['Server_Settings_info']; ?></h5>
 </h4>
+<?php
+}
+?>
