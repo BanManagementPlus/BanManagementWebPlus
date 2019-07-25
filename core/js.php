11c3a149e17761a8ff94582edae77765bbd77c55
Mryan <40586009+Mryan2005@users.noreply.github.com>
2019-07-25 20:37:52 +0800

    fix js_online

--- core/js.php
+++ core/js.php
@@ -4,10 +4,11 @@
 # [1.9]jquery.validate.min.js
 #$js['jquery_validate'] = 'https://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js';
 $js_online = array(
-    'https://cdn.staticfile.org/jquery/3.4.1/core.js',
+    'https://www.jq22.com/jquery/jquery-3.3.1.js',
     'https://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js',
     'https://www.jq22.com/jquery/bootstrap-4.2.1.js',
     'https://www.jq22.com/jquery/html5.min.js',
     'https://www.jq22.com/jquery/jquery-migrate-1.2.1.min.js',
     'https://cdn.staticfile.org/jquery.tablesorter/2.31.1/js/extras/jquery.tablesorter.pager.min.js',
+    'https://cdn.staticfile.org/jquery/3.4.1/core.js',
 );
